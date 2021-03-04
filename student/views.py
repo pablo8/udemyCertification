4b5ab6e22db07c60d9a60b99bf8456cc90dcdfbb
Your Name <pablom.ochoteco@gmail.com>
2021-03-04 19:50:58 -0300

    add mixin and generics

--- student/views.py
+++ student/views.py
@@ -4,6 +4,33 @@ from rest_framework.response import Response
 from rest_framework import status
 from student.serializers import StudentSerializer
 from student.models import Student
+from rest_framework import mixins, generics
+
+
+class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class DetailStudent(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request,pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
 
 
 class ListingStudent(APIView):
